5ed72f1c049cf9e5156ab0d3e08ab17273e802a2
ns2772 <bytelogic772@gmail.com>
2024-01-08 18:54:23 -0500

    first commit

---
+++ src/models/Burrito.ts
@@ -0,0 +1,13 @@
+class Burrito {
+  name: string;
+  size: string;
+  price: number;
+
+  constructor(name: string, size: string, price: number) {
+      this.name = name;
+      this.size = size;
+      this.price = price;
+  }
+}
+
+export default Burrito;
